83dad272733735c256bcd565f4ca340daa2eb2e3
surfacebook\Ryuki Kobayashi <kobayashiryuki1002@gmail.com>
2021-01-03 23:19:39 -0600

    set random timer / set random choice / set counter / stop when swiping out

--- tinderSwipe.js
+++ tinderSwipe.js
@@ -1,10 +1,28 @@
-let buttons = document.querySelectorAll(".focus-button-style");
-let likeButton = buttons[buttons.length - 2];
+const confirmBox = confirm('Start swiping right?');
+const buttons = document.querySelectorAll(".focus-button-style");
+const likeButton = buttons[buttons.length - 2];
+const dislikeButton = buttons[buttons.length - 4];
+
+let counter = 0;
 
 function like() {
-    likeButton.click();
-    let interval = 1500 + Math.random() * 1000;
-    setTimeout(like, interval);
+    if (likeButton.disabled) {
+        alert(`Congrats! You swiped out!\n \
+               You swiped ${counter} girls.`);
+        console.log(`The number of swipe ${couter}`);
+    } else {
+        let random = Math.random();
+        if (random < 0.98) {
+            counter += 1;
+            likeButton.click();
+        } else {
+            dislikeButton.click();
+        }
+        let interval = 1500 + Math.random() * 1000;
+        setTimeout(like, interval);
+    }
 }
 
-like();
+if (confirmBox) {
+    like();
+}
